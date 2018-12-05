89f2589b4a6651715ec9d15b32189b93912c37af
youssefiibrahim <youssefibrahim.guc@gmail.com>
2018-12-05 12:47:49 +0100

    learning lifecycles and correspondence

---
+++ src/components/post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+const Post = () => {
+    return (
+        <div>
+            <Link to="/posts/1">post 1</Link><br/>
+            <Link to="/posts/2">post 2</Link><br/>
+            <Link to="/posts/3">post 3</Link><br/>       
+        </div>
+    )
+}
+
+export default Post
--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route, NavLink, Switch} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// COMPONENTS
+import Home from './components/home';
+import Posts from './components/post';
+import Profile from './components/profile';
+import PostItem from './components/post_item';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// myawesomeapp.com/posts
+// myawesomeapp.com/profile/posts
+const App = () =>{
+    return (
+        <BrowserRouter>
+            <div>
+                <header>
+                    <NavLink to="/">Home</NavLink><br />
+                    <NavLink 
+                        to="/posts"
+                        activeClassName="selected"
+                    >Posts</NavLink><br />
+                    <NavLink to={{
+                        pathname: '/profile'
+                    }}>Profile</NavLink><br />
+                    <hr />
+                </header>
+                <Switch>
+                    <Route path="/posts/:id/:username" component={PostItem}/>
+                    <Route path="/posts" component={Posts}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/" exact component={Home}/>
+                    <Route render={()=> <h3>Ooops 404</h3>}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
